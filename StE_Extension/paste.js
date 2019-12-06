7e9945bdcbb5c6e176dd5c119d1aa51c63a78faa
tang0376 <tang0376@umn.edu>
2019-12-06 11:45:19 -0600

    add support for latest msg, interaction location

--- StE_Extension/paste.js
+++ StE_Extension/paste.js
@@ -1,5 +1,19 @@
 'use strict';
 
+function findLocation(name, link, visibility) {
+  if (name == "FACEBOOK") {
+    //TODO: wasn't able to find a wall post to test. Not sure if this is working correctly
+    if (visibility == "Private") {return "PM / DM";}
+    else if (link.includes("comment")) {return "Comment / Tweet";}
+    else {return "Wall Post";}
+  } else if (name == "TWITTER") {
+    if (visibility == "Private") {return "PM / DM";}
+    else {return "Comment / Tweet";}
+  } else {
+    return "";
+  }
+}
+
 function autofill() {
   //TODO: error handling
 
@@ -7,38 +21,14 @@ function autofill() {
   chrome.storage.local.get("CaseData", function(result) {
     let infoCase = result["CaseData"].data[0];
 
-    // Date of Customer Post
-    // format 11/15/2019
-    let date = new Date(infoCase.createdTime);
-    let dateOfCustomer = document.querySelector("#Left36");
-    dateOfCustomer.readOnly = false;
-    dateOfCustomer.value = date.toLocaleDateString();
-    dateOfCustomer.readOnly = true;
-
-    // Time of Customer Post
-    // format 12:00
-    let time = date.toLocaleTimeString();
-    let secondColonIndex = time.indexOf(":", 4);
-    document.querySelector("#igtxtLeft37").value = time.substr(0, secondColonIndex);
-    // AM or PM
-    // format PM
-    let mIndex = time.indexOf("M");
-    document.querySelector("#Left37_ampm").value = time.substr(mIndex-1, mIndex);;
-
     //Tonality
     document.querySelector("#Memo64").value = infoCase.workflow.customFields["5c3f7f66e4b00ecbb3523869"];
 
-    //Interaction Location
-    document.querySelector("#Memo71").value = "Comment / Tweet";
+    // //Customer Number (if known)
+    // document.querySelector("#Memo41").value = "000001";
 
-    //Customer Number (if known)
-    document.querySelector("#Memo41").value = "000001";
-
-    //Customer's Post
-    document.querySelector("#steps_to_reproduce").value = infoCase.description;
-
-    //Link to Interaction
-    document.querySelector("#Right40").value = "https://www.sprinklr.com";
+    // //Link to Interaction
+    // document.querySelector("#Right40").value = "https://www.sprinklr.com";
   });
 
   chrome.storage.local.get("ProfileData", function(result) {
@@ -54,6 +44,35 @@ function autofill() {
 
   });
 
+  chrome.storage.local.get("MsgData", function(result) {
+    let infoMsg = result["MsgData"].data;
+
+    // Date of Customer Post
+    // format 11/15/2019
+    let date = new Date(infoMsg.createdTime);
+    let dateOfCustomer = document.querySelector("#Left36");
+    dateOfCustomer.readOnly = false;
+    dateOfCustomer.value = date.toLocaleDateString();
+    dateOfCustomer.readOnly = true;
+
+    // Time of Customer Post
+    // format 12:00
+    let time = date.toLocaleTimeString();
+    let secondColonIndex = time.indexOf(":", 4);
+    document.querySelector("#igtxtLeft37").value = time.substr(0, secondColonIndex);
+    // AM or PM
+    // format PM
+    let mIndex = time.indexOf("M");
+    document.querySelector("#Left37_ampm").value = time.substr(mIndex-1, mIndex);;
+
+    //Customer's Post
+    document.querySelector("#steps_to_reproduce").value = infoMsg.content.text;
+
+    //Interaction Location
+    document.querySelector("#Memo71").value = findLocation(infoMsg.channelType, infoMsg.permalink, infoMsg.workflow.customFields["5c490fd3e4b0afd92c3e6a7a"][0]);
+
+  });
+
   //now that the data has been used, remove it from local storage
   chrome.storage.local.remove(["CaseData"], function() {
     console.log("deleted CaseData JSON.");
@@ -63,6 +82,9 @@ function autofill() {
     console.log("deleted ProfileData JSON.");
   });
 
+  chrome.storage.local.remove(["MsgData"], function() {
+    console.log("deleted MsgData JSON.");
+  });
 
 }
 
