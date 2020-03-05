949a3065531c810826eab79f522061b848aa8380
tang0376 <tang0376@umn.edu>
2020-03-05 14:32:03 -0600

    add conditional check
    wrap actions in if-statement so program fills what JSON has without stopping in the middle

--- AutoTicket/paste.js
+++ AutoTicket/paste.js
@@ -41,73 +41,83 @@ function autofill() {
   chrome.storage.local.get("MsgData", function(result) {
     let msgInfo = result["MsgData"].data;
 
-    // replace link with its screen name if link is not
-    // 1. url or 2. starts with @
-    if (msgInfo.hasOwnProperty('textEntities')){
-      if(msgInfo.textEntities.hasOwnProperty('message')){
-        if(msgInfo.textEntities.message[0].hasOwnProperty('screenName')){
-          let replaceStart = msgInfo.textEntities.message[0].indices[0];
-          let replaceEnd = msgInfo.textEntities.message[0].indices[1];
-          let replaceWord = msgInfo.textEntities.message[0].screenName;
-          let msgLength = msgInfo.content.text.length;
-          if(msgInfo.content.text.substring(start, start+1) != "@"){
-            msgInfo.content.text = msgInfo.content.text.substring(0, start) + replaceWord + msgInfo.content.text.substring(end, msgLength);
+  // replace link with its screen name if link is not
+  // 1. url or 2. starts with @
+  if (msgInfo.hasOwnProperty('textEntities')){
+    if(msgInfo.textEntities.hasOwnProperty('message')){
+      if(msgInfo.textEntities.message[0].hasOwnProperty('screenName')){
+        let replaceStart = msgInfo.textEntities.message[0].indices[0];
+        let replaceEnd = msgInfo.textEntities.message[0].indices[1];
+        let replaceWord = msgInfo.textEntities.message[0].screenName;
+        let msgLength = msgInfo.content.text.length;
+        if(msgInfo.content.text.substring(replaceStart, replaceStart+1) != "@"){
+          msgInfo.content.text = msgInfo.content.text.substring(0, replaceStart) + replaceWord + msgInfo.content.text.substring(replaceEnd, msgLength);
+        }
+        //Customer's Post
+        document.querySelector("#steps_to_reproduce").value = msgInfo.content.text;
+      }
+    }
+  }
+
+  // Date of Customer Post
+  // format 11/15/2019
+  let date = new Date(msgInfo.createdTime);
+  let dateOfCustomer = document.querySelector("#Left36");
+  let datestring = ('0' + (date.getMonth()+ 1)).slice(-2) + '/' +
+                  ('0' + date.getDate()).slice(-2) + '/' +
+                  date.getFullYear();
+  dateOfCustomer.readOnly = false;
+  dateOfCustomer.value = datestring;
+  dateOfCustomer.readOnly = true;
+
+  // Time of Customer Post
+  // format 12:00
+  let time = date.toLocaleTimeString();
+  let secondColonIndex = time.indexOf(":", 4);
+  document.querySelector("#igtxtLeft37").value = time.substr(0, secondColonIndex);
+  document.querySelector("#igtxtLeft37").select();
+  window.getSelection().removeAllRanges();
+
+  // AM or PM
+  // format PM
+  let mIndex = time.indexOf("M");
+  document.querySelector("#Left37_ampm").value = time.substr(mIndex-1, mIndex);
+
+  //Date of Sleep Number Reply
+  let dateOfReplyField = document.querySelector("#Left38");
+  let replyDate = new Date();
+  let replyDateString = ('0' + (replyDate.getMonth()+ 1)).slice(-2) + '/' +
+                        ('0' + replyDate.getDate()).slice(-2) + '/' +
+                        replyDate.getFullYear();
+  dateOfReplyField.readOnly = false;
+  dateOfReplyField.value = replyDateString;
+  dateOfReplyField.readOnly = true;
+
+  if (msgInfo.hasOwnProperty('enrichments')){
+    if (msgInfo.enrichments.hasOwnProperty('sentiment')){
+      //Tonality
+      document.querySelector("#Memo64").value = intToSentiment(msgInfo.enrichments.sentiment);
+    }
+  }
+
+  if (msgInfo.hasOwnProperty('channelType') && msgInfo.hasOwnProperty('permalink')){
+        //Social Media Channel
+        document.querySelector("#Memo34").value = formatChannelName(msgInfo.channelType, msgInfo.permalink);
+        if (msgInfo.hasOwnProperty('workflow')){
+          if (msgInfo.workflow.hasOwnProperty('customProperties')){
+            if (msgInfo.workflow.customProperties.hasOwnProperty('5c490fd3e4b0afd92c3e6a7a')){
+              //Interaction Location
+              document.querySelector("#Memo71").value = findLocation(msgInfo.channelType, msgInfo.permalink, msgInfo.workflow.customProperties["5c490fd3e4b0afd92c3e6a7a"][0]);
+            }
           }
         }
       }
-    }
-
-    // Date of Customer Post
-    // format 11/15/2019
-    let date = new Date(msgInfo.createdTime);
-    let dateOfCustomer = document.querySelector("#Left36");
-    let datestring = ('0' + (date.getMonth()+ 1)).slice(-2) + '/' +
-                    ('0' + date.getDate()).slice(-2) + '/' +
-                    date.getFullYear();
-    dateOfCustomer.readOnly = false;
-    dateOfCustomer.value = datestring;
-    dateOfCustomer.readOnly = true;
-
-    // Time of Customer Post
-    // format 12:00
-    let time = date.toLocaleTimeString();
-    let secondColonIndex = time.indexOf(":", 4);
-    document.querySelector("#igtxtLeft37").value = time.substr(0, secondColonIndex);
-    document.querySelector("#igtxtLeft37").select();
-    window.getSelection().removeAllRanges();
-
-    // AM or PM
-    // format PM
-    let mIndex = time.indexOf("M");
-    document.querySelector("#Left37_ampm").value = time.substr(mIndex-1, mIndex);
-
-    //Date of Sleep Number Reply
-    let dateOfReplyField = document.querySelector("#Left38");
-    let replyDate = new Date();
-    let replyDateString = ('0' + (replyDate.getMonth()+ 1)).slice(-2) + '/' +
-                          ('0' + replyDate.getDate()).slice(-2) + '/' +
-                          replyDate.getFullYear();
-    dateOfReplyField.readOnly = false;
-    dateOfReplyField.value = replyDateString;
-    dateOfReplyField.readOnly = true;
-
-    //Tonality
-    document.querySelector("#Memo64").value = intToSentiment(msgInfo.enrichments.sentiment);
 
     //AutoTicket checkbox
     document.querySelector("#Memo84").checked = true;
 
-    //Social Media Channel
-    document.querySelector("#Memo34").value = formatChannelName(msgInfo.channelType, msgInfo.permalink);
-
-    //Interaction Location
-    document.querySelector("#Memo71").value = findLocation(msgInfo.channelType, msgInfo.permalink, msgInfo.workflow.customProperties["5c490fd3e4b0afd92c3e6a7a"][0]);
-
     //Customer Number (if known)
     //document.querySelector("#Memo41").value = "000001";
-
-    //Customer's Post
-    document.querySelector("#steps_to_reproduce").value = msgInfo.content.text;
 
     //Link to Interaction
     //document.querySelector("#Right40").value = "https://www.sprinklr.com";
