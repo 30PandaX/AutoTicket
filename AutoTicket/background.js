54b83ae6769a0a25450a8f6ba68a57728fae7d46
tang0376 <tang0376@umn.edu>
2020-03-26 10:25:22 -0500

    Revert "add support for listening sourceType"
    This reverts commit 86770ba60377c34b464d81569c2495cdf0bb8b0f.

--- AutoTicket/background.js
+++ AutoTicket/background.js
@@ -14,15 +14,10 @@ async function apiCall(msgID) {
     }
   };
   //now we are ready to make the API call
-  var msgResponse = await fetch('https://api2.sprinklr.com/api/v2/message?sourceType=ACCOUNT&id='+msgID, config);
-  var msgInfo = await msgResponse.json();
+  let msgResponse = await fetch('https://api2.sprinklr.com/api/v2/message?sourceType=ACCOUNT&id='+msgID, config);
+  let msgInfo = await msgResponse.json();
 
-  if (msgInfo.hasOwnProperty('errors')) {
-    if (msgInfo.errors[0].code == 404) {
-      msgResponse = await fetch('https://api2.sprinklr.com/api/v2/message?sourceType=LISTENING&id='+msgID, config);
-      msgInfo = await msgResponse.json();
-    }
-  }
+  console.log(msgInfo);
 
   chrome.storage.local.set({MsgData: msgInfo}, function() {
     console.log("Stored msgInfo JSON.");
